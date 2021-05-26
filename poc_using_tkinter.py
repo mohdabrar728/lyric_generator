6d053d2b6e40157ed4619c2b98e521b4eab728e1
mohdabrar728 <37308585+mohdabrar728@users.noreply.github.com>
2021-05-26 14:34:18 +0530

    Add files via upload

--- poc_using_tkinter.py
+++ poc_using_tkinter.py
@@ -1,4 +1,5 @@
 import os
+from time import sleep
 from tkinter import *
 import pygame
 root = Tk()
@@ -6,9 +7,6 @@ path = 'songs'
 song_list = os.listdir(path)
 
 pygame.mixer.init()  # initialise the pygame
-def play():
-    pygame.mixer.music.load("mp3/kya.mp3")
-    pygame.mixer.music.play(loops=0)
 root.geometry("600x400")
 
 title = Label(root, text="Music Player", bd=9, relief=GROOVE,
@@ -29,11 +27,21 @@ def CurSelet(event):
     if picked != unpicked[-1]:
         lyricer.configure(text=picked)
         left.configure(text=lyric_print)
+        def play():
+            print("played")
+            pygame.mixer.music.load(f"mp3/{picked}.mp3")
+            pygame.mixer.music.play(loops=0)
+            for i in lyric_print.split('\n'):
+                print(i)
+                sleep(3)
+        b.configure(command=play)
     unpicked.append(picked)
 listbox.bind('<<ListboxSelect>>',CurSelet)
 listbox.pack(side=LEFT, fill=Y)
 scrollbar = Scrollbar(root)
 scrollbar.pack( side = RIGHT, fill = Y )
+b = Button(root,text="submit")
+b.pack(side=BOTTOM)
 canvas = Canvas(root,yscrollcommand = scrollbar.set)
 canvas.pack(fill='both')
 scrollbar.config(command = canvas.yview )
